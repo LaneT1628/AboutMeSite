5c2f33192c78664f2f384c9dc435be069e9a27c9
LaneT1628 <lane.turner1628@gmail.com>
2024-11-16 21:07:05 -0500

    Initial Setup

---
+++ README.md
@@ -0,0 +1,19 @@
+# Infosite
+This website is intended to be an ongoing project created by Lane Turner
+
+### Objectives
+- Learn/practice/master front end coding skills and languages like
+    - html
+    - css
+- Create a new personal project to showcase coding skills
+- A centralized location to display all information related to Lane Turner
+
+### Disclaimers
+#### Contributors
+The code in this project was modelled and based off of the following repo
+    - https://owickstrom.github.io/the-monospace-web/
+
+#### Images
+The logos used in this website are not mine, and are based on the fair usage policies of both LinkedIn™ and Instagram™. 
+- https://brand.linkedin.com/policies#:~:text=If%20you%20have%20a%20current,the%20terms%20of%20the%20agreement
+- https://about.meta.com/brand/resources/instagram/instagram-brand/
---
+++ index.js
@@ -0,0 +1,70 @@
+// Global variable to track the currently selected section
+let currentlyClickedLink = null;
+
+function showSection(sectionId, clickedLink) {
+    // Show only clicked section
+    const sections = document.querySelectorAll('section');
+    const selectionSection = document.getElementById(sectionId);
+    sections.forEach(section => {
+        if (section === selectionSection) {
+            section.style.display = 'flex';
+        }
+        else {
+            section.style.display = 'none';
+        }
+    });
+
+    // Remove the underline from the previous section
+    if (currentlyClickedLink != null) {
+        currentlyClickedLink.style.textDecoration = 'none';
+    }
+
+    // Add the underline to the clicked link
+    clickedLink.style.textDecoration = 'underline';
+
+    // Set the global current link to the clicked
+    currentlyClickedLink  = clickedLink;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const homeLink = document.getElementById('homebutton');
+    currentlyClickedLink = homeLink;
+    homeLink.style.textDecoration = 'underline';
+});
+
+// Add event listeners permanently for hover effect
+const links = document.querySelectorAll('#headers a');
+links.forEach(link => {
+    // Add hover effect for links (except for the already clicked one)
+    link.addEventListener('mouseenter', function() {
+        // Only underline the link if it is not the clicked link
+        if (link != currentlyClickedLink) {
+            link.style.textDecoration = 'underline';
+        }
+    });
+
+    link.addEventListener('mouseleave', function() {
+        // Only remove underline if it is not the clicked link
+        if (link != currentlyClickedLink) {
+            link.style.textDecoration = 'none';
+        }
+    });
+});
+
+/* Creating a dynamic slide show for introduction */
+/* Slide show taken from https://www.w3schools.com/howto/howto_js_slideshow.asp */
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.querySelectorAll("#slideshow img");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex-1].style.display = "flex";
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
